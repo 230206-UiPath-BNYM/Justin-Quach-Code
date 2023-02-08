cb44af4e3b47eaadcc91e2a023d5b75d7603bad7
justquack <43820200+justquack@users.noreply.github.com>
2023-02-08 09:10:19 -0800

    Added GuessingGame

--- Week1/vbout/Program.vb
+++ Week1/vbout/Program.vb
@@ -5,6 +5,7 @@ Module Program
         dim greeting as String = "hey"
         dim greetStyle as String = "casual" 'casual, business casual, full business, etc'
         Console.WriteLine(greeter(greetStyle))
+        GuessingGame(1)
     End Sub
     Function greeter(greeting_type As String) As String
         Select Case greeting_type
@@ -18,4 +19,12 @@ Module Program
                 Return "hello"
         End Select
     End Function
+    Sub GuessingGame(guess as Integer)
+        dim rnd As Random = new Random()
+        dim mysteryNumber as integer = rnd.next(1,2)
+        If guess <> mysteryNumber Then '<> is for not equal'
+            Console.WriteLine("Wrong guess :<")
+        Else Console.WriteLine("Good guess :>")
+        End If
+    End Sub
 End Module
